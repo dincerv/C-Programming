da88edc9f74977880481a090909a029664b06433
Dinçer <dincer.velioglu@hotmail.com>
2020-07-20 16:15:26 +0300

    q2

---
+++ PatternPrinting/q2/q2.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main(void)
+{
+	int numOfRows, rows, cols;
+	printf("Enter number of rows to be printed.\n");
+	scanf("%d", &numOfRows);
+
+	int numOfColToPrint = numOfRows;
+
+	for (rows = 1; rows <= numOfRows; rows++)
+	{
+		for (cols = 1; cols <= numOfColToPrint; cols++)
+		{
+			printf("*");
+		}
+		printf("\n");
+		numOfColToPrint--;
+	}
+
+	return (0);
+}
